fc5e51efe4966329df3034075850cd5d769b62af
FaabsD <fabian.et.je@hotmail.com>
2023-02-21 15:07:57 +0100

    Created methods for rendering a settings page with a setting whether to display collection in REST API Route or not

--- collector.php
+++ collector.php
@@ -16,26 +16,34 @@ if ( ! function_exists( 'get_plugin_data' ) ) {
 
 class CollectorPlugin {
 
-	public $pluginData;
-	public $pluginUri;
-	public $pluginTextDomain;
+	public array $pluginData;
+	public string $pluginUri;
+	public mixed $pluginTextDomain;
+
+	private array $defaultSettings;
 
 	public function __construct() {
 		$this->pluginData       = get_plugin_data( __FILE__ );
 		$this->pluginUri        = $this->pluginData['PluginURI'];
 		$this->pluginTextDomain = $this->pluginData['TextDomain'];
+		$this->defaultSettings  = array(
+			'open_to_rest' => 0,
+		);
 
 		add_action( 'init', array( $this, 'collectorSetup' ) );
 
 		register_activation_hook( __FILE__, array( $this, 'activate' ) );
 	}
 
-	public function collectorSetup() {
+	public function collectorSetup(): void {
 		$this->registerPosttypes();
+		add_action( 'admin_init', [ $this, 'options' ] );
+		$this->settingsPage();
 
 	}
 
-	private function registerPosttypes() {
+	private function registerPosttypes(): void {
+		$options = get_option( 'collector_options', $this->defaultSettings );
 		register_post_type( 'collection', array(
 			'labels'       => array(
 				'name'               => __( 'Collection', $this->pluginTextDomain ),
@@ -57,22 +65,97 @@ class CollectorPlugin {
 			'menu_icon'    => 'dashicons-media-archive',
 			'rewrite'      => array( 'with_front' => true ),
 			'supports'     => array( 'title', 'editor', 'excerpt' ),
-			'show_in_rest' => true,
+			'show_in_rest' => ( $options['open_to_rest'] === 1 ) ? true : false,
 		) );
 	}
 
-	private function unregisterPosttypes( array $posttypes ) {
+	private function unregisterPosttypes( array $posttypes ): void {
 		foreach ( $posttypes as $posttype ) {
 			unregister_post_type( $posttype );
 		}
 	}
 
-	public function activate() {
+	/**
+	 *
+	 * @return void
+	 */
+	private function settingsPage(): void {
+		add_options_page( 'Collection Settings', 'Collection Settings', 'manage_options', 'collection_options', function () {
+			echo '<h2>FD Projects Settings</h2>';
+			echo '<form action="options.php" method="post">';
+			settings_fields( 'collector_options' );
+			do_settings_sections( 'Collector' );
+			echo '<input type="submit" value="' . esc_attr( 'Save' ) . '" name="submit" class="button button-primary">';
+			echo '</form>';
+		} );
+	}
+
+	public function options(): void {
+		$fields = array(
+			array(
+				'field_id'       => 'open_to_rest',
+				'field_text'     => 'Show in Rest API',
+				'field_callback' => 'renderCheckbox',
+				'page'           => 'Collector',
+				'section'        => 'collector_general_settings',
+			),
+		);
+
+		register_setting(
+			'collector_options',
+			'collector_options',
+			[ $this, 'validateCollectorSettings' ] );
+		add_settings_section(
+			'collector_general_settings',
+			'General settings',
+			[ $this, 'generalSettingsText' ],
+			'Collector'
+		);
+
+		// add fields
+		foreach ( $fields as $field ) {
+			add_settings_field(
+				$field['field_id'],
+				__( $field['field_text'], $this->pluginTextDomain ),
+				[ $this, $field['field_callback'] ],
+				$field['page'],
+				$field['section'],
+				array( 'field_id' => $field['field_id'] )
+			);
+		}
+
+	}
+
+	public function validateCollectorSettings( $input ): array {
+		$output['open_to_rest'] = absint( $input['open_to_rest'] );
+
+		return $output;
+	}
+
+	public function generalSettingsText(): void {
+		echo '<p>From here you can set the general options</p>';
+	}
+
+	public function renderCheckbox( $args ): void {
+		if ( defined( 'WP_DEBUG' ) ) {
+			error_log( '======== DEBUG LOG FOR RENDERING CHECKBOXES ON SETTINGS PAGE FOR COLLECTOR PLUGIN ========' );
+			error_log( print_r( $args, true ) );
+		}
+
+		$options = get_option( 'collector_options', $this->defaultSettings );
+
+		$html = '<input type="checkbox" name="collector_options[' . $args['field_id'] . ']"' . checked( $options[ $args['field_id'] ], 1, false ) . ' value="1"/>';
+
+		echo $html;
+
+	}
+
+	public function activate(): void {
 		$this->registerPosttypes();
 		flush_rewrite_rules();
 	}
 
-	public function deactivate() {
+	public function deactivate(): void {
 		$this->unregisterPosttypes( array( 'collection' ) );
 		flush_rewrite_rules();
 	}
